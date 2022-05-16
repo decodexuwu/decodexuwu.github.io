d529370e2e07a1cdcd01b4f010e8f85ed57cd7b3
Decode-Chan <decodexps4@gmail.com>
2022-05-15 23:42:43 -0400

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function toggleSettings() {
+    setMenu.classList.toggle("active");
+}
+
+function toggleProfWindow() {
+    menuWindow.classList.toggle("active");
+}
+
+function toggleStartWindow() {
+    battleWindow.classList.toggle("active");
+}
+
+function callTitle(title) {
+    title = menuTitle;
+}
+
+/**
+ *         <div id="windWrap"> 
+        <div class="windTitle"> </div>
+            <div class="windowSub"> </div>
+        </div>
+ * 
+ */
